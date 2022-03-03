a5e5731f917cc02cdeb80b09bdac495b312a4541
Hisan Ibrahim <hisanibrahim@gmail.com>
2022-03-03 15:41:31 +0530

    integrated api

--- src/App.js
+++ src/App.js
@@ -1,250 +1,51 @@
-const data = {
-  term: "new york",
-  moresuggestions: 10,
-  autoSuggestInstance: null,
-  trackingID: "98dccfdef4674106aa7fd74dc9249200",
-  misspellingfallback: false,
-  suggestions: [
-    {
-      group: "CITY_GROUP",
-      entities: [
-        {
-          geoId: "2621",
-          destinationId: "1506246",
-          landmarkCityDestinationId: null,
-          type: "CITY",
-          redirectPage: "DEFAULT_PAGE",
-          latitude: 40.75668,
-          longitude: -73.98647,
-          searchDetail: null,
-          caption:
-            "<span class='highlighted'>New</span> <span class='highlighted'>York</span>, New York, United States of America",
-          name: "New York",
-        },
-        {
-          geoId: "129440",
-          destinationId: "1535616",
-          landmarkCityDestinationId: null,
-          type: "NEIGHBORHOOD",
-          redirectPage: "DEFAULT_PAGE",
-          latitude: 40.77827894430704,
-          longitude: -73.96990559824117,
-          searchDetail: null,
-          caption:
-            "Manhattan, <span class='highlighted'>New</span> <span class='highlighted'>York</span>, New York, United States of America",
-          name: "Manhattan",
-        },
-        {
-          geoId: "6203489",
-          destinationId: "1737998",
-          landmarkCityDestinationId: null,
-          type: "NEIGHBORHOOD",
-          redirectPage: "DEFAULT_PAGE",
-          latitude: 40.7520567817666,
-          longitude: -73.9874924492131,
-          searchDetail: null,
-          caption:
-            "Midtown, <span class='highlighted'>New</span> <span class='highlighted'>York</span>, New York, United States of America",
-          name: "Midtown",
-        },
-        {
-          geoId: "6234083",
-          destinationId: "11108016",
-          landmarkCityDestinationId: null,
-          type: "REGION",
-          redirectPage: "DEFAULT_PAGE",
-          latitude: 43.04737748563339,
-          longitude: -75.65465713202158,
-          searchDetail: null,
-          caption:
-            "Upstate <span class='highlighted'>New</span> <span class='highlighted'>York</span>, New York, United States of America",
-          name: "Upstate New York",
-        },
-        {
-          geoId: "149004",
-          destinationId: "1633594",
-          landmarkCityDestinationId: null,
-          type: "NEIGHBORHOOD",
-          redirectPage: "DEFAULT_PAGE",
-          latitude: 40.71025273032951,
-          longitude: -73.81875841072811,
-          searchDetail: null,
-          caption:
-            "Queens, <span class='highlighted'>New</span> <span class='highlighted'>York</span>, United States of America",
-          name: "Queens",
-        },
-        {
-          geoId: "6157019",
-          destinationId: "1634459",
-          landmarkCityDestinationId: null,
-          type: "NEIGHBORHOOD",
-          redirectPage: "DEFAULT_PAGE",
-          latitude: 40.75852956154528,
-          longitude: -73.98571085482959,
-          searchDetail: null,
-          caption:
-            "Theater District, <span class='highlighted'>New</span> <span class='highlighted'>York</span>, New York, United States of America",
-          name: "Theater District",
-        },
-        {
-          geoId: "6360640",
-          destinationId: "1912147",
-          landmarkCityDestinationId: null,
-          type: "REGION",
-          redirectPage: "DEFAULT_PAGE",
-          latitude: 40.72015720913082,
-          longitude: -73.9987452185321,
-          searchDetail: null,
-          caption:
-            "Lower Manhattan, <span class='highlighted'>New</span> <span class='highlighted'>York</span>, United States of America",
-          name: "Lower Manhattan",
-        },
-      ],
-    },
-    {
-      group: "HOTEL_GROUP",
-      entities: [
-        {
-          geoId: "15838",
-          destinationId: "115902",
-          landmarkCityDestinationId: null,
-          type: "HOTEL",
-          redirectPage: "DEFAULT_PAGE",
-          latitude: 40.758234,
-          longitude: -73.985457,
-          searchDetail: null,
-          caption:
-            "<span class='highlighted'>New</span> <span class='highlighted'>York</span> Marriott Marquis, New York, New York, United States of America",
-          name: "New York Marriott Marquis",
-        },
-        {
-          geoId: "19768",
-          destinationId: "112922",
-          landmarkCityDestinationId: null,
-          type: "HOTEL",
-          redirectPage: "DEFAULT_PAGE",
-          latitude: 40.76225,
-          longitude: -73.97874,
-          searchDetail: null,
-          caption:
-            "<span class='highlighted'>New</span> <span class='highlighted'>York</span> Hilton Midtown, New York, New York, United States of America",
-          name: "New York Hilton Midtown",
-        },
-      ],
-    },
-    {
-      group: "LANDMARK_GROUP",
-      entities: [
-        {
-          geoId: "502080",
-          destinationId: "1409835",
-          landmarkCityDestinationId: "1506246",
-          type: "LANDMARK",
-          redirectPage: "DEFAULT_PAGE",
-          latitude: 40.757410161691084,
-          longitude: -73.98618346732785,
-          searchDetail: null,
-          caption:
-            "Times Square, <span class='highlighted'>New</span> <span class='highlighted'>York</span>, New York, United States of America",
-          name: "Times Square",
-        },
-        {
-          geoId: "6291778",
-          destinationId: "124",
-          landmarkCityDestinationId: "1506246",
-          type: "LANDMARK",
-          redirectPage: "DEFAULT_PAGE",
-          latitude: 40.75059,
-          longitude: -73.993503,
-          searchDetail: null,
-          caption:
-            "Madison Square Garden, <span class='highlighted'>New</span> <span class='highlighted'>York</span>, New York, United States of America",
-          name: "Madison Square Garden",
-        },
-        {
-          geoId: "6056463",
-          destinationId: "1707357",
-          landmarkCityDestinationId: "1506246",
-          type: "LANDMARK",
-          redirectPage: "DEFAULT_PAGE",
-          latitude: 40.78255312101171,
-          longitude: -73.96547122696427,
-          searchDetail: null,
-          caption:
-            "Central Park, <span class='highlighted'>New</span> <span class='highlighted'>York</span>, New York, United States of America",
-          name: "Central Park",
-        },
-      ],
-    },
-    {
-      group: "TRANSPORT_GROUP",
-      entities: [
-        {
-          geoId: "4933194",
-          destinationId: "1662393",
-          landmarkCityDestinationId: null,
-          type: "AIRPORT",
-          redirectPage: "DEFAULT_PAGE",
-          latitude: 40.644166,
-          longitude: -73.782548,
-          searchDetail: null,
-          caption:
-            "<span class='highlighted'>New</span> <span class='highlighted'>York</span>, NY, United States of America (JFK-John F. Kennedy Intl.)",
-          name: "New York, NY (JFK-John F. Kennedy Intl.)",
-        },
-        {
-          geoId: "4278092",
-          destinationId: "1412655",
-          landmarkCityDestinationId: null,
-          type: "AIRPORT",
-          redirectPage: "DEFAULT_PAGE",
-          latitude: 40.77429,
-          longitude: -73.872035,
-          searchDetail: null,
-          caption:
-            "<span class='highlighted'>New</span> <span class='highlighted'>York</span>, NY, United States of America (LGA-LaGuardia)",
-          name: "New York, NY (LGA-LaGuardia)",
-        },
-        {
-          geoId: "6185978",
-          destinationId: "1730462",
-          landmarkCityDestinationId: null,
-          type: "TRAIN_STATION",
-          redirectPage: "DEFAULT_PAGE",
-          latitude: 40.75056,
-          longitude: -73.994609,
-          searchDetail: null,
-          caption:
-            "Penn Station, <span class='highlighted'>New</span> <span class='highlighted'>York</span>, New York, United States of America",
-          name: "Penn Station",
-        },
-      ],
-    },
-  ],
-  geocodeFallback: false,
-};
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const App = () => {
-  return (
-    <div className="App">
-      {data.suggestions.map((value, key) => {
-        return (
-          <>
-            <table>
-              <h4>{value.group}</h4>
-              {value.entities.map((value, key) => {
-                return (
-                  <>
-                    <tr>{`${value.name}`}</tr>
-                  </>
-                );
-              })}
-            </table>
-          </>
-        );
-      })}
-    </div>
-  );
+  const [apiData, setApiData] = useState({});
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const getData = async () => {
+      const config = {
+        method: "get",
+        url: "https://hotels4.p.rapidapi.com/locations/v2/search?query=new york",
+        headers: {
+          "x-rapidapi-host": "hotels4.p.rapidapi.com",
+          "x-rapidapi-key": process.env.REACT_APP_API_KEY,
+        },
+      };
+
+      const response = await axios(config);
+      setApiData(response.data);
+      setLoading(false);
+    };
+
+    getData();
+  }, []);
+
+  if (loading) return <p>Loading...</p>;
+  else
+    return (
+      <div className="App">
+        {apiData.suggestions.map((value, key) => {
+          return (
+            <>
+              <table>
+                <h4>{value.group}</h4>
+                {value.entities.map((value, key) => {
+                  return (
+                    <>
+                      <tr>{`${value.name}`}</tr>
+                    </>
+                  );
+                })}
+              </table>
+            </>
+          );
+        })}
+      </div>
+    );
 };
 
 export default App;
